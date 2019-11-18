7b2c2ffcf3edb84646c93ce97d8a55679941627b
Regina <regishan.08@gmail.com>
2019-11-18 13:46:12 -0800

    add

--- fractol.c
+++ fractol.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rnureeva <rnureeva@student.42.fr>          +#+  +:+       +#+        */
+/*   By: reginanureeva <reginanureeva@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 18:08:50 by rnureeva          #+#    #+#             */
-/*   Updated: 2019/11/18 12:00:52 by rnureeva         ###   ########.fr       */
+/*   Updated: 2019/11/18 13:43:42 by reginanuree      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+#include <stdio.h>
 
 void put_num(t_fractol *image)
 {
@@ -20,13 +21,17 @@ void put_num(t_fractol *image)
 	double y;
 	double x_new;
 
-	max = 200;
+	printf("(1)\n");
+	max = 20;
+	image->height = 100;
+	image->width = 100;
 	image->row = 0;
 	image->col = 0;
 	while (image->row < image->height)
 	{
 		while (image->col < image->width)
 		{
+			// printf("(2)\n");
 			image->c_real = (image->col - image->width / 2) * 4 / image->width;
 			image->c_imag = (image->row - image->height / 2) * 4 / image->width;
 			x = 0;
@@ -35,16 +40,21 @@ void put_num(t_fractol *image)
 			while (x * x + y * y <= 4 && i < max)
 			{
 				x_new = x * x - y * y + image->c_real;
-				y = 2 * x * y + image->c_real;
+				y = 2 * x * y + image->c_imag;
 				x = x_new;
 				i++;
 			}
+			// printf("(%d, %d): %d\n", image->row, image->col, i);
+			// if (image->row == 200 && image->col == 200)
+			// 	printf("(%d, %d): %d\n", image->row, image->col, i);
 			if (i < max)
-				mlx_pixel_put(image->mlx, image->window, image->col, image->row,0xffffff);
+				mlx_pixel_put(image->mlx, image->window, image->col, image->row, 0xffffff);
 			else
-				mlx_pixel_put(image->mlx, image->window, image->col, image->row,0xfffff);
+				mlx_pixel_put(image->mlx, image->window, image->col, image->row, 0xfffff);
 			image->col++;
 		}
+		printf("(%d, %d): %d\n", image->row, image->col, i);
+		image->col = 0;
 		image->row++;
 	}
 }
@@ -53,14 +63,14 @@ int main(void)
 {
 	t_fractol *image;
 
-	image = (t_fractol*)malloc(sizeof(t_fractol));
+	image = (t_fractol *)malloc(sizeof(t_fractol));
 
 	image->mlx = mlx_init();
 	image->window = mlx_new_window(image->mlx, 1000, 1000, "Title");
 	// image->window = mlx_new_image(image->mlx,1000,1000);
 	put_num(image);
-	mlx_pixel_put(image->mlx, image->window, 500,100,0xffffff);
+	// mlx_pixel_put(image->mlx, image->window, 500,100,0xffffff);
 	// mlx_new_image(image->mlx, 500, 500);
 	mlx_loop(image->mlx);
-	return(0);
+	return (0);
 }
