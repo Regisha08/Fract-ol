7afaa149212a9b59d27914eb5826200dd14349b2
Regina <regishan.08@gmail.com>
2019-11-18 14:18:48 -0800

    add

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: reginanureeva <reginanureeva@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 18:08:50 by rnureeva          #+#    #+#             */
-/*   Updated: 2019/11/18 13:43:42 by reginanuree      ###   ########.fr       */
+/*   Updated: 2019/11/18 13:59:46 by reginanuree      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,38 +22,34 @@ void put_num(t_fractol *image)
 	double x_new;
 
 	printf("(1)\n");
-	max = 20;
-	image->height = 100;
-	image->width = 100;
+	max = 100;
+	image->height = 500;
+	image->width = 500;
 	image->row = 0;
 	image->col = 0;
 	while (image->row < image->height)
 	{
 		while (image->col < image->width)
 		{
-			// printf("(2)\n");
 			image->c_real = (image->col - image->width / 2) * 4 / image->width;
-			image->c_imag = (image->row - image->height / 2) * 4 / image->width;
+			image->c_imag = (image->row - image->height / 2) * 4 / image->height;
 			x = 0;
 			y = 0;
 			i = 0;
-			while (x * x + y * y <= 4 && i < max)
+			while (x * x + y * y <= 16 && i < max)
 			{
 				x_new = x * x - y * y + image->c_real;
 				y = 2 * x * y + image->c_imag;
 				x = x_new;
 				i++;
 			}
-			// printf("(%d, %d): %d\n", image->row, image->col, i);
-			// if (image->row == 200 && image->col == 200)
-			// 	printf("(%d, %d): %d\n", image->row, image->col, i);
 			if (i < max)
 				mlx_pixel_put(image->mlx, image->window, image->col, image->row, 0xffffff);
 			else
 				mlx_pixel_put(image->mlx, image->window, image->col, image->row, 0xfffff);
 			image->col++;
 		}
-		printf("(%d, %d): %d\n", image->row, image->col, i);
+		// printf("(%d, %d): %d\n", image->row, image->col, i);
 		image->col = 0;
 		image->row++;
 	}
